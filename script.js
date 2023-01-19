40ebc2f704a94e3584eca36fe3d280b83c64b7cd
unknown <bruno.ximendes42@gmail.com>
2023-01-19 14:40:46 -0300

    bore

---
+++ script.js
@@ -0,0 +1,21 @@
+function togglemode(){
+
+const html = document.documentElement /*Document é a representação em formato de objeto em JS, o que vem depois do ponto
+é o que acessa propriedades e funcionalidades */
+
+const img = document.querySelector('#profile img')/*selecionao SELETOR TODO do css, digitar TODO o seletor*/ 
+
+if (html.classList.contains('L')){
+        html.classList.remove('L')
+        img.setAttribute('src', './assets/avl.png')
+    
+
+} else {
+
+html.classList.add('L')
+img.setAttribute('src', './assets/avatar.png')
+
+}
+
+
+}
