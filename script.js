fe1698f21cab72afb3d08acf1a086820dca773a1
BrunoDoCarmo28 <123109344+BrunoDoCarmo28@users.noreply.github.com>
2023-01-19 23:47:13 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,17 +5,6 @@ const html = document.documentElement /*Document é a representação em formato
 html.classList.toggle('L')
 const img = document.querySelector('#profile img')/*selecionao SELETOR TODO do css, digitar TODO o seletor*/ 
 
-if (html.classList.contains('L')){
-       /* html.classList.remove('L')*/
-        img.setAttribute('src', 'Assets/avatar.png');
-    
-
-} else {
-
-/*html.classList.add('L')*/
-img.setAttribute('src', 'Assets/avl.png')
-
-}
 
 
 }
