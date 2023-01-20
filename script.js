73cdfc79bc93ae56c31f9391567a0f7326bdd7d7
unknown <bruno.ximendes42@gmail.com>
2023-01-19 23:13:44 -0300

    fixing JS

--- script.js
+++ script.js
@@ -2,18 +2,18 @@ function togglemode(){
 
 const html = document.documentElement /*Document é a representação em formato de objeto em JS, o que vem depois do ponto
 é o que acessa propriedades e funcionalidades */
-
-const img = document.querySelector('#profile img')/*selecionao SELETOR TODO do css, digitar TODO o seletor*/ 
+html.classList.toggle('L')
+const img = document.querySelector("#profile img")/*selecionao SELETOR TODO do css, digitar TODO o seletor*/ 
 
 if (html.classList.contains('L')){
-        html.classList.remove('L')
-        img.setAttribute('src', './assets/avl.png')
+       /* html.classList.remove('L')*/
+        img.setAttribute('src', './assets/avatar.png')
     
 
 } else {
 
-html.classList.add('L')
-img.setAttribute('src', './assets/avatar.png')
+/*html.classList.add('L')*/
+img.setAttribute('src', './assets/avl.png')
 
 }
 
