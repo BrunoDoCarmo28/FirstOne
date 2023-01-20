af6124202d1128f656d9de01cd6a280cfaf72e4e
BrunoDoCarmo28 <123109344+BrunoDoCarmo28@users.noreply.github.com>
2023-01-19 23:39:25 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function togglemode(){
 const html = document.documentElement /*Document é a representação em formato de objeto em JS, o que vem depois do ponto
 é o que acessa propriedades e funcionalidades */
 html.classList.toggle('L')
-const img = document.querySelector("#profile img")/*selecionao SELETOR TODO do css, digitar TODO o seletor*/ 
+const img = document.querySelector('#profile img')/*selecionao SELETOR TODO do css, digitar TODO o seletor*/ 
 
 if (html.classList.contains('L')){
        /* html.classList.remove('L')*/
