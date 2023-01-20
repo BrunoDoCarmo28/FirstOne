1fcffdc3a7f241d48ea27f7c9694ce4d46f5cb13
BrunoDoCarmo28 <123109344+BrunoDoCarmo28@users.noreply.github.com>
2023-01-20 00:11:27 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 function togglemode(){
 
-const html = document.documentElement 
+const html = document.documentElement
+
 html.classList.toggle("L")
-const img = document.querySelector("#profile img")
+   
+const img = document.querySelector("#profile img")/*selecionao SELETOR TODO do css, digitar TODO o seletor*/ 
 
 if (html.classList.contains("L")){
-   img.setAttribute("src", "./assets/avatar.png")
+       
+ img.setAttribute("src", "./assets/avatar.png")
     
 
 } else {
