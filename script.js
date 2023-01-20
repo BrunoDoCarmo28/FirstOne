082589fe3e2e0813edfc061f4910e34a56d9baf2
BrunoDoCarmo28 <123109344+BrunoDoCarmo28@users.noreply.github.com>
2023-01-19 23:55:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
-function togglemode(){
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("L")
 
-const html = document.documentElement /*Document é a representação em formato de objeto em JS, o que vem depois do ponto
-é o que acessa propriedades e funcionalidades */
-html.classList.toggle('L')
-const img = document.querySelector('#profile img')/*selecionao SELETOR TODO do css, digitar TODO o seletor*/ 
+  // pegar a tag img
+  const img = document.querySelector("#profile img")
 
-
-
+  // substituir a imagem
+  if (html.classList.contains("light")) {
+    // se tiver light mode, adicionar a imagem light
+    img.setAttribute("src", "./assets/avl.png")
+  } else {
+    // set tiver sem light mode, manter a imagem normal
+    img.setAttribute("src", "./assets/avatar.png")
+  }
 }
